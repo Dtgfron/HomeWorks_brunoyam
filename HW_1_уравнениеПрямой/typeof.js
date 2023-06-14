e6531ef07f3120d5c81f30d2854a2d04685a4532
Valeriya Mokhnina <135635789+Dtgfron@users.noreply.github.com>
2023-06-15 00:04:55 +0300

    typeof.js

--- HW_1_уравнениеПрямой/typeof.js
+++ HW_1_уравнениеПрямой/typeof.js
@@ -1,1 +1,23 @@
+let a = 4;
+console.log (typeof a); // "number"
 
+let b = 'Петя';
+console.log (typeof b); // "String"
+
+let c = 123n;
+console.log (typeof c); // "bigint"
+
+let d = 2 < 3;
+console.log (typeof d); // "boolean"
+
+let e = Symbol()
+console.log (typeof e); // "symbol"
+
+let f = {};
+console.log (typeof f); // "object"
+
+let g = null;
+console.log (typeof g); // "object", т.к. видит null как объект (ошибка)
+
+let h = function () {};
+console.log (typeof h); // "function"
